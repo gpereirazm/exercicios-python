00d4f3e58be2b4451272db2bbd695bea389ead10
Guilherme Pereira da Silva Filho <guilherminhupereira07@gmail.com>
2025-08-12 16:24:05 -0300

    Create solução concluída do Verificando se a Lista está Vazia.py

---
+++ solução concluída do Verificando se a Lista está Vazia.py
@@ -0,0 +1,11 @@
+def esta_vazia(lista):
+    if len(lista) == 0:
+        return True
+    else:
+        return False
+      
+lista1 = []
+lista2 = [1, 2, 3]
+
+print(esta_vazia(lista1)) 
+print(esta_vazia(lista2)) 
